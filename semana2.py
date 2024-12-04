6c7e1220ac74c30a3ace71c15bf5a236067e3fc1
Lucas dos Santos Pimentel <116539949+LucasPimentel77@users.noreply.github.com>
2024-12-04 19:47:41 -0400

    Delete semana2.py

--- semana2.py
+++
@@ -1,9 +0,0 @@
-idade = float(input("digite sua idade: "))
-
-if idade > 65:
-    print("oba")
-    print("ja pode se aposentar!!!!!")
-else:
-    print("ainda não pode se apresentar :c")
-
-print(f"sua idade = {idade}")
